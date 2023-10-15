adcc7650448462eb7a2d90e6c48647d9ce1d69bb
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-15 18:08:25 -0300

    Convert map to a columnar format.

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 
  const tmx = require('tmx-parser');
  
+ const arrayOfSize = n => Array(n).fill();
+ 
  tmx.parseFile('original/pocket-platformer.level-3.tmx', function(err, map) {
-  if (err) throw err;
-  console.log('map', map.layers[0].tiles);
+	if (err) throw err;
+
+	const { width, height } = map;
+	
+	const layer = map.layers[0];
+	const columns = arrayOfSize(height).map((_, y) => arrayOfSize(width).map((_, x) => {
+		const tile = layer.tileAt(x, y);
+		return tile ? tile.gid : 0;
+	}));
+
+	console.log({ width, height, columns });	  
 });
