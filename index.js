78565844fc5d992e584401c5735c3ed784bc314b
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-15 17:52:02 -0300

    Use `tmx-parser` to read the maps.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
 
+ const tmx = require('tmx-parser');
+ 
+ tmx.parseFile('original/pocket-platformer.level-3.tmx', function(err, map) {
+  if (err) throw err;
+  console.log('map', map.layers[0].tiles);
+});
