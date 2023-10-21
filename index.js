a5d9c194fede9f0e4c8bbed8673ed21c2b5c191a
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-10-21 08:06:29 -0300

    Find object list.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const tmx = require('tmx-parser');
 
 const arrayOfSize = n => Array(n).fill();
+
+const findMapLayerOfType = (map, type) => map.layers.find(layer => layer.type == type);
  
 tmx.parseFile('original/pocket-platformer.level-3.tmx', function(err, map) {
 	if (err) throw err;
@@ -13,11 +15,13 @@ tmx.parseFile('original/pocket-platformer.level-3.tmx', function(err, map) {
 	
 	// The map will be converted into a columnar format, to make the horizontal scrolling faster
 	
-	const layer = map.layers[0];
+	const layer = findMapLayerOfType(map, 'tile');
 	const columns = arrayOfSize(width).map((_, x) => arrayOfSize(height).map((_, y) => {
 		const tile = layer.tileAt(x, y);
 		return tile ? tile.gid : 0;
 	}));
+	
+	console.log(findMapLayerOfType(map, 'object').objects);
 		
 	const buffer = Buffer.from([width, height, ...columns.flat()], 'application/octet-stream');
 	fs.writeFileSync('experiment/data/map3.bin', buffer, 'binary');
